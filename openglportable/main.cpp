eebcbf893dacd263915b8fa1f3feb1cd79e50856
Zishan Ahmed Onik <zishan.onik@aiub.edu>
2025-12-24 15:32:33 +0600

    remove color

--- openglportable/main.cpp
+++ openglportable/main.cpp
@@ -4,16 +4,9 @@ void display()
 {
     glClear(GL_COLOR_BUFFER_BIT);
     glBegin(GL_POLYGON);
-        glColor3f(0, 0, 1);
         glVertex2f(0.5, -0.5);
-
-        glColor3f(0, 1, 0);
         glVertex2f(-0.5, -0.5);
-
-        glColor3f(0, 1, 1);
         glVertex2f(-0.5, 0.5);
-
-        glColor3f(1, 0, 0);
         glVertex2f(0.5, 0.5);
     glEnd();
     glFlush();
