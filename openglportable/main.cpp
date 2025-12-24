795fb428b57b9b0c8db4ce45bbb9bd24582b5708
Zishan Ahmed Onik <zishan.onik@aiub.edu>
2025-12-24 15:36:02 +0600

    adding red color

--- openglportable/main.cpp
+++ openglportable/main.cpp
@@ -4,6 +4,7 @@ void display()
 {
     glClear(GL_COLOR_BUFFER_BIT);
     glBegin(GL_POLYGON);
+        glColor3f(1, 0, 0);
         glVertex2f(0.5, -0.5);
         glVertex2f(-0.5, -0.5);
         glVertex2f(-0.5, 0.5);
